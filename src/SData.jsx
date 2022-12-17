b7fb099f92b65188cba345d72d57ae13f4e5c733
Anjali182003 <103763691+Anjali182003@users.noreply.github.com>
2022-12-17 10:07:50 +0530

    Add files via upload

---
+++ src/SData.jsx
@@ -0,0 +1,26 @@
+const SData = [
+  {
+    id: 1,
+    imgsrc: "https://play-lh.googleusercontent.com/nwyAuT0LWsS06vZNzstE-XsXcrwiLhWo8wp6ICMUfHwdwQbENJ5-bqhP3V96QudLIOmOWQ",
+    category: "Animated Cartoon Show",
+    title: "Bal Ganesh",
+    link: "https://youtu.be/Gvi4uUj9o18"
+  },
+  {
+    id: 2,
+    imgsrc: "https://upload.wikimedia.org/wikipedia/en/e/e1/Doraemon_2015.jpg",
+    category: "Animated Cartoon Show",
+    title: "Doraemon and nobita the explorer bow bow",
+    link: "https://youtu.be/6D8Ghs1j07Y"
+  },
+  {
+    id: 3,
+    imgsrc: "https://bollyspice.com/wp-content/uploads/2021/02/LegendOfHanuman_Poster_05-612x897.jpg",
+    category: "Animated Cartoon Show", 
+    title: "The Legend of Hanuman",
+    link: "https://youtu.be/2a_CDlWCt-Q"
+  },
+  
+];
+
+export default SData;
